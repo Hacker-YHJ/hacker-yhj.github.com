86b94a1f24574c90bb864bd019fbe837f82df5a7
YangHJ <jscnm1993@gmail.com>
2014-02-15 17:14:54 +0800

    refine dot-navi

--- projects/resume/js/dot-navi.js
+++ projects/resume/js/dot-navi.js
@@ -2,19 +2,53 @@
   var
     $frames = $('div.frame'),
     $navi = $('div.dot-navi'),
-    $dots;
-
+    $dots,
+    windowTop,
+    windowBottom,
+    current,
+    previous;
+  $frames.first().css('height', $(window).height());
   $frames.each( function (i) {
-    $navi.append('<div class="dot"><div class="description">' +
+    $navi.append('<div class="dot-container" no=' + i +
+      '><div class="dot"></div><div class="description">' +
       $(this).find('h1').text() +
       '</div></div>');
   });
 
-  $dots = $('div.navi>div.dots');
+  $dots = $('div.dot-navi div.dot');
 
-  $dots.on( 'click', function (e, i) {
-    console.log(e, i);
+  $dots.on( 'click', function (e) {
+    console.log(e);
     console.log(this);
   });
-  $dots.click();
+
+  $(window).scroll( function () {
+    windowTop = $(window).scrollTop();
+    windowBottom = windowTop + $(window).height();
+    $frames.each( function (i) {
+      thisTop = $(this).offset().top;
+      thisBottom = thisTop + $(this).height();
+      if (thisBottom - windowTop < 300) { return; }
+      else if (windowBottom - thisTop < 0) { return; }
+      else {
+        previous = current;
+        current = i;
+        return false;
+      }
+    });
+    if (previous !== current) $dots.eq(previous).removeClass('current');
+    $dots.each( function (i) {
+      if (current > i) {
+        $(this).css('bottom', ($navi.height()-(i+2)*20));
+      }
+      else if (current === i) {
+        $(this).css('bottom', '50%');
+        if (previous !== current) $(this).addClass('current');
+      }
+      else if (current < i) {
+        $(this).css('bottom', ($frames.length-i)*20);
+      }
+    });
+  });
+  $(window).scroll();
 })();
